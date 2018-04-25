9e20637bd92d2d073569314652a31bc134453cd1
Alain Plattner <alainplattner@gmail.com>
2018-04-25 16:24:27 -0700

    added damping to itweighres

--- itweighres.m
+++ itweighres.m
@@ -1,16 +1,32 @@
-function [slepcoef,dataweights]=itweighres(M,data,slepcoef,niter)
+function [slepcoef,dataweights,dampweights]=itweighres(M,data,slepcoef,niter,lambda,slepcoef0)
 % Iteratively reweighted residual solver based on 
 % Farquharson and Oldenburg (1998)
+%
+% Use eq (23--26) in their paper. 
+%
+% 4/23/2018: Now with damping
+
+defval('lambda',0)
+defval('slepcoef0',zeros(size(slepcoef)))
 
 defval('rthresh',(1e-4)*median(abs(data(:))))
+defval('rthresh_s',(1e-4)*median(abs(slepcoef(:))))
 % Here is the weighted residual iteration
 weights=[];
+weights_s=[];
 for iter=1:niter       
     residuals=data-M'*slepcoef;      
     residuals(abs(residuals)<rthresh)=rthresh;
+
+    % For the damping, we need different residuals
+    residuals_s=slepcoef-slepcoef0;
+    residuals_s(abs(residuals_s)<rthresh_s)=rthresh_s;
+
     % Put the inverse of the thresheld residuals as diagonal elements
     % in the weighting matrix    
     weights=1./abs(residuals);
+
+    weights_s=1./abs(residuals_s);
 
     % Here I am reweighing the data points
     % I think I can't get around having a second matrix and a second
@@ -37,7 +53,11 @@ for iter=1:niter
     % And now solve the reweighted problem: 
     % Farquharson and Oldenburg (1998) eq. (23):
     % (G'*R*G)m=G'*R*d 
-    slepcoef = (M*Mp')\(M*datap);
+    % slepcoef = (M*Mp')\(M*datap);
+    % With damping:
+    % (lambda*Rs + G'*R*G)m = lambda*Rs*m0 + G'*R*d
+    slepcoef = (lambda*diag(weights_s) + M*Mp')\(lambda*weights_s.*slepcoef0 + M*datap);
     fprintf('iteration step %d: weighted thresheld residual norm is %g\n',iter,norm(residuals)/sqrt(length(data)))
 end
 dataweights=weights;
+dampweights=weights_s;
